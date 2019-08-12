7a14603e09f24a009dee2b9a16a6935869a3ade9
Michael McDonnell <Michael.McDonnell067@gmail.com>
2019-08-12 16:56:16 -0500

    loop excercise

---
+++ js/for-loops.js
@@ -0,0 +1,30 @@
+
+
+// function showMultiplication(num) {
+//     for (var i = 1; i <= 10; i++){
+//
+//         console.log(num + 'X' + i + '=' + (num * i));
+//
+//     }
+//
+// }
+//
+// showMultiplication(2);
+
+
+// for(var i = 1; i<=10 ;i++){
+//     var num = Math.floor(Math.random() * 200);
+//     if(num % 2 === 0){
+//         console.log(num + " is Even");
+//     }else{
+//         console.log(num + " is Odd");
+//     }
+// }
+
+// for (var i = 100; i > 10; i-= 5){
+// //     console.log(i)
+// // }
+
+for (var i = 1; i <= 9; i++){
+    console.log(i.toString().repeat(i))
+}
---
+++ js/while.js
@@ -0,0 +1,37 @@
+// function multiplyBy2() {
+// var i = 2
+//
+// while (i < 70000) {
+//      console.log(i);
+//     i = i * 2;
+// }
+//
+// }
+// multiplyBy2();
+
+// function iceCream() {
+//     var allCones = Math.floor(Math.random() * 50) + 50;
+//
+//     do {
+//     var conesPerPerson = Math.floor(Math.random() * 5) + 1;
+//         allCones = allCones - conesPerPerson;
+//         if (allCones > conesPerPerson) {
+//             console.log('if theres enough cones ' + conesPerPerson);
+//         }else if (allCones < conesPerPerson) {
+//             console.log('Sorry i dont have that many ' + allCones)
+//         }else {
+//             console.log('Sold out')
+//         }
+//
+//     }while (allCones > 0)
+// }
+//
+//
+// iceCream();
+//
+// var i = 0;
+//
+// do {
+//     console.log('while loop iteration #' + i);
+//     i++;
+// } while (i < 10);
