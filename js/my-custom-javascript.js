041faf674fa36185565edaaad76cbbdca1c72d13
Michael McDonnell <Michael.McDonnell067@gmail.com>
2019-08-23 12:24:22 -0500

    selectors

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -2,6 +2,27 @@
 
 $(document).ready(function() {
 
-    alert('The page is done loading')
+    // alert('The page is done loading')
+    //
+    // var contents = $('#main').html();
+    // alert(contents);
+    //
+    // var contents = $('#unorder').html();
+    // alert(contents);
+    //
+    // $('.codeup').css('border', '1px solid red');
+
+    // $('li').css('font-size', '20px');
+    //
+    // $('h1').css('background', '#ffcccc');
+    //
+    // $('li').css('background', '#ffcccc');
+    //
+    // $('p').css('background', '#ffcccc');
+    //
+    // var contents = $('#header').html();
+    //     alert(contents);
+
+    $('h1, li, p').css('background', '#ffcccc');
 
 });
