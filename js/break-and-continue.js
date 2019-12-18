4b978c4293482808e0747235218f42e079ee10b7
Michael McDonnell <Michael.McDonnell067@gmail.com>
2019-12-18 12:28:28 -0600

    removed commented out code in break-and-continue.js

--- js/break-and-continue.js
+++ js/break-and-continue.js
@@ -13,15 +13,3 @@ for (var i = 1; i < 50; i+=2){
     }
     console.log(i)
 }
-
-
-
-// var personInput = prompt('pick an odd number between 1 and 50');
-// for (var i = 1; i <= 50; i+=2) {
-//     if (i == personInput) {
-//         console.log('yikes ' + personInput)
-//         continue;
-//
-//     }
-//     console.log('odd numbers ' + i)
-// }
